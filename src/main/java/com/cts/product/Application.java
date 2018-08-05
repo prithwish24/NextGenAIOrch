437d977fb778b290b577af1bf58cd5c3fb5a8f09
Prithwish Ghosh <prithwish.ghosh@cognizant.com>
2018-08-06 00:22:57 +0530

    Enable spring MVC servlet.

--- src/main/java/com/cts/product/Application.java
+++ src/main/java/com/cts/product/Application.java
@@ -3,13 +3,14 @@ package com.cts.product;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.Banner;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
+@SpringBootApplication
 @ComponentScan (basePackages={"com.cts.product"})
-public class Application {
+public class Application extends WebMvcAutoConfiguration {
 	public static final Logger LOG = LoggerFactory.getLogger(Application.class);
 	
 	public static void main(String[] args) {
