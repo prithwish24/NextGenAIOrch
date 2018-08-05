d493f9a1e37c6423aa1ded4c4462b6bc588df024
Tanmoy Biswas <tanmoy.b81@gmail.com>
2018-08-05 16:11:37 +0530

    snake case

---
+++ src/main/java/com/cts/product/config/JacksonConfiguration.java
@@ -0,0 +1,16 @@
+package com.cts.product.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+
+@Configuration
+public class JacksonConfiguration {
+
+    @Bean
+    public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
+	return new Jackson2ObjectMapperBuilder().propertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+    }
+}
