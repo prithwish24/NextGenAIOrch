4bd7ad2c380395b020b6c8821999e8ab3a937d33
Prithwish Ghosh <prithwish.ghosh@cognizant.com>
2018-08-09 15:22:42 +0530

    removing MVC related changes

--- src/main/java/com/cts/product/Application.java
+++ src/main/java/com/cts/product/Application.java
@@ -4,13 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan (basePackages={"com.cts.product"})
-public class Application extends WebMvcAutoConfiguration {
+//@ComponentScan (basePackages={"com.cts.product"})
+public class Application {
 	public static final Logger LOG = LoggerFactory.getLogger(Application.class);
 	
 	public static void main(String[] args) {
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html lang="en">
+<head>
+	<link rel="stylesheet" href="/resources/css/style.css">
+	<link rel="stylesheet" href="/resources/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/resources/css/bootstrap-theme.min.css">
+</head>
+<body>
+
+	<div class="container">
+        <div class="jumbotron">
+            <h2>${title}</h2>
+            <p>${message}</p>
+        </div>
+    </div>
+
+<script type="text/javascript" src="/resources/js/app.js"></script>
+<script type="text/javascript" src="/resources/js/bootstrap.min.js"></script>
+</body>
+</html>
